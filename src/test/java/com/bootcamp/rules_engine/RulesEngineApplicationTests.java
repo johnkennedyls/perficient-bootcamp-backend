b21b5908f6b19ddb9a2afccc9692d21166145aa5
Laura-Martinez182 <71099708+Laura-Martinez182@users.noreply.github.com>
2023-06-24 11:32:56 -0500

    First commit

---
+++ src/test/java/com/bootcamp/rules_engine/RulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.rules_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
